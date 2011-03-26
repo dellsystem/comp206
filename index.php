2e0912ead65f77d19363962e6ec1e4797b898e90
dellsystem <ilostwaldo@gmail.com>
2011-03-26 15:52:36 -0400

    made the welcome page open in a new tab/window lolololol

--- index.php
+++ index.php
@@ -78,7 +78,7 @@ if ( !isset($title) ) {
             <ul>
                 <li><a href="index.php">Home</a></li>
                 <li><a href="index.php?mode=login">Login</a></li>
-                <li><a href="index.php?mode=credits">Credits</a></li>
+                <li><a href="index.php?mode=credits" target="_blank"><!-- not exactly good practice but WHATEVER don't judge me -->Credits</a></li>
             </ul>
             <div id="clock">
               <applet CODE="LiteClockApp.class" 
