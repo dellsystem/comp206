88a28dcb83222105eeadd5041765adaa197ba230
dellsystem <ilostwaldo@gmail.com>
2011-03-27 20:44:53 -0400

    changed right URL to another group, fixed up my html for the moon

--- index.php
+++ index.php
@@ -117,7 +117,7 @@ if ( !isset($title) ) {
                 include('room5.html');
                 $menu = true;
                 $left_url = 'index.php?mode=room4';
-                $right_url = 'http://www.cs.mcgill.ca/~tbatze/';
+                $right_url = 'http://cs.mcgill.ca/~asaif/';
                 break;
             default:
                 include('home.html');
