04c4869e894f60f3061e6d109c8fc45e7e1fb17e
dellsystem <ilostwaldo@gmail.com>
2011-04-01 22:10:24 -0400

    moved more template files to layout/, created credits.py and login.py

---
+++ login.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+# this rudimentary template engine does all the header/footer printing shit, to avoid duplication
+import template
+
+page_title = "Login"
+
+# Call the function for displaying the header - pass it the title
+template.header(page_title)
+
+# Call the function for showing the content - pass it the name of the html file
+template.content('login')
+
+template.footer()
+
