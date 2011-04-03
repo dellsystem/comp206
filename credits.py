4c04ffcd3288bd6095de7825c75eaf410eeb23ec
Harry Brundage <harry.brundage@gmail.com>
2011-04-03 16:32:08 -0400

    Move the pre-login pages to the footer-less layout

--- credits.py
+++ credits.py
@@ -1,4 +1,4 @@
 #!/usr/bin/env python
 # this rudimentary template engine does all the header/footer printing shit, to avoid duplication
 import template
-template.render("credits", dict(page_title="Credits"))
+template.render("credits", dict(page_title="Credits", layout="logged_out"))
