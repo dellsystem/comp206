7f4f9d271e05c6ce2eff2c96d955cee42e0c34a7
Harry Brundage <harry.brundage@gmail.com>
2011-04-05 01:12:19 -0400

    Added a basic implementation of the user inventory business, activity.py is going to be like 4 lines, god.

--- game.py
+++ game.py
@@ -1,6 +1,6 @@
 # game.py - implementation of the commodity trading game including inventory reading and writing
 
-import random
+import random, re
 
 # Class which represents the metaclass of the available items for purchase
 # Responsible for managing the global particulars of one entry, such as the price generation
@@ -8,10 +8,11 @@ class Commodity:
     def __init__(self, name, prices):
         self.name = name
         self.prices = prices
-    
+
     # Used for initializing a Planet's market (see Planet)
-    def toInventoryItem(self):
-        return InventoryItem(self.name, 0, random.choice(self.prices)) 
+    def toInventoryDict(self):
+        return {'name': self.name, 'quantity': 0, 'price': random.choice(self.prices), 'available_quantity': 0}
+
 
 # Class which represents the possession of some commodity by the planet. A row in the inventory, if you will.
 # Holds the name of the commodity, the quantity possesed by the planet, and the price at this instant.
@@ -25,13 +26,13 @@ class InventoryItem:
             self.commodity = commodities[0]
         else:
             self.commodity_name = "Space Junk (%s)" % commodity_name
-    
+
     def updateQnty( self, qnty_change ):
         self.quantity += qnty_change
-        
+
     def getCSV( self ):
         csv = '%s,%d,\n' % (self.commodity_name, self.quantity)
-        return csv      
+        return csv
 
 # Class which represents the collection of currently available items, and manages reading and writing this to disk
 class PlanetInventory:
@@ -44,7 +45,7 @@ class PlanetInventory:
         inventory = inv_file.readlines()
         inv_file.close()
 
-        self.items = [] 
+        self.items = []
         for entry in inventory:
             entry.strip('\n')
             values = entry.split(',')
@@ -55,48 +56,92 @@ class PlanetInventory:
 
     def write(self):
         inv_file = open(self.filename, 'w')
-    
+
         for item in self.items:
             line = item.getCSV()
             inv_file.write(line)
-                        
+
         inv_file.close()
         return self.items
 
+# Raised when the form tag describing a user's inventory isn't correctly formatted
+class MalformedGameFormError(Exception):
+    pass
+
+class UserInventory:
+    def __init__(self, form):
+        self.items = []
+
+        if "points" not in form:
+            raise MalformedGameFormError()
+        else:
+            self.points = form["points"].value
+
+        # Add all the inventory items we can to this object
+        for key in form:
+            if re.search('Inventory(\d+)', key):
+                value = form[key].value
+                matches = re.search('^(\d+) (.+)$', value)
+                if m:
+                      # Matches our iventory spec, yahoo! Figure out what commodity it is, and add an
+                      # Inventory Item. If the commodity name is recognized (see the init fn for
+                      # InventoryItem), then the price sent here will be ignored. Otherwise, we pick
+                      # a small random value for Space Junk
+                      self.items.append(InventoryItem(m.group(2), m.group(1), random.choice([1,2,3])))
+                else:
+                      # Does not match our inventory spec. Take a tiny quantity and a random price.
+                      self.items.append(InventoryItem(value, 1, random.choice([1,2,3])))
+
+    def render(self):
+        s = '<input type="hidden" name="points" value=""' + str(self.points) + '">'
+        for i, item in enumerate(self.items):
+            s += '<input type="hidden" name="Inventory' + str(i+1) + '" value="' + str(item.quantity) + '">'
+        return s
+
 # Class which represnets an inventory full of items which may or may not be part of the commodities
 class Planet:
-    def __init__(self):
-        self._market = {}
+    def __init__(self, user_inventory):
+        self.market = {}
         self.inventory = PlanetInventory()
-        
+        self.user_inventory = user_inventory
         # The market has all the standard commodities by default.
         for com in Commodities:
-            self._market[com.name] = com.toInventoryItem()
+            self.market[com.name] = com.toInventoryDict()
 
-        # Loop through the items in the users inventory. If its a standard item, then
+        # Loop through the items in the planet's inventory. If its a standard item, then
         # add the quantity to the existing item. If its non standard, add it to the list.
+        # Notice that here, the quantity of the inventory item represents the quantity
+        # the planet has to sell to the user, so we put it in the 'available_quantity' key.
         for item in self.inventory.items:
-            if item.commodity_name in self._market:
-                self._market[item.commodity_name]['quantity'] += item.quantity
+            if item.commodity_name in self.market:
+                self.market[item.commodity_name]['available_quantity'] += item.quantity
             else:
-                self._market[item.commodity_name] = item
+                self.market[item.commodity_name] = {'name': item.commodity_name, 'quantity': 0, 'price': item.price, 'available_quantity': item.quantity}
 
-    # Creates a uniform representation of the commodities possesed by the user and available for purchase.
-    # This is what the view needs to render.
-    def market(self):
-        # TODO: implement available_quantity for each planet
-        return [{'name': x.commodity_name, 'quantity':x.quantity, 'price':x.price, 'available_quantity': 200} for k, x in self._market.iteritems()]
+        # Loop through the items in the user's inventory. If its a standard item, then
+        # add the quantity to the existing item. If its non standard, add it to the list.
+        # Notice that here, the quantity of the inventory item represents the quantity the
+        # user has to sell, so we put it in the 'quantity' key.
+        for item in self.user_inventory.items:
+            if item.commodity_name in self.market:
+                self.market[item.commodity_name]['quantity'] += item.quantity
+            else:
+                self.market[item.commodity_name] = {'name': item.commodity_name, 'quantity': item.quantity, 'price': item.price, 'available_quantity': 0}
+
+        self.market = [row for key, row in self.market.iteritems()]
+
+
 
 # Constant listing all the rooms and their attributes
 Rooms = [{'name': "The Moon", 'title': "The Moon", 'image':"moon_thumb", 'url':"~wliu65/206/"},
-         {'name': "Arrakis", 'title': "Arrakis", 'image':"arrakis_thumb", 'url':"~hbrund/206/"},
-         {'name': "Orion", 'title': "The Orion Nebula", 'image':"orion_thumb", 'url':"~csuder/206/"},
-         {'name': "SSS", 'title': "Shatner Space Station", 'image':"sss_thumb", 'url':"~cleung24/206/"},
-         {'name': "MM", 'title': "Memento Mori", 'image':"mm_thumb", 'url':"~ezarou/206/"}]
+      {'name': "Arrakis", 'title': "Arrakis", 'image':"arrakis_thumb", 'url':"~hbrund/206/"},
+      {'name': "Orion", 'title': "The Orion Nebula", 'image':"orion_thumb", 'url':"~csuder/206/"},
+      {'name': "SSS", 'title': "Shatner Space Station", 'image':"sss_thumb", 'url':"~cleung24/206/"},
+      {'name': "MM", 'title': "Memento Mori", 'image':"mm_thumb", 'url':"~ezarou/206/"}]
 
 # Constant listing the default commodities available on every planet, and the range of prices each one might adopt
-Commodities = map(lambda x: Commodity(x[0],x[1]), [["Ore",          range(10,20)],
-                                                   ["Laser Swords", range(100,200)], 
-                                                   ["Cheese",       range(2,4)], 
-                                                   ["Melange",      range(1000,6000)], 
-                                                   ["Whatever",     range(10,20)]])
+Commodities = map(lambda x: Commodity(x[0],x[1]), [["Ore",         range(10,20)],
+                                                  ["Laser Swords", range(100,200)],
+                                                  ["Cheese",       range(2,4)],
+                                                  ["Melange",      range(1000,6000)],
+                                                  ["Whatever",     range(10,20)]])
--- show.py
+++ show.py
@@ -3,55 +3,62 @@
 # Usage: Visit /show.py?room=# where # is the room number being shown
 import template, game, cgi
 
-# Figure out the room number and grab the room info
-form = cgi.FieldStorage()
-# Note: "room" is a POST variable, not GET anymore
-room_number = int(form.getfirst("room", 1)) # Default of the first room
-room = game.Rooms[room_number-1]
+try:
+    # Figure out the room number and grab the room info
+    form = cgi.FieldStorage()
+    # Note: "room" is a POST variable, not GET anymore
+    room_number = int(form.getfirst("room", 1)) # Default of the first room
+    room = game.Rooms[room_number-1]
 
-# Planet class does all the backend stuff.
-planet = game.Planet()
+    # Get the user's inventory.
+    # IF THE USER HAS GOTTEN HERE BY LEGITIMATE MEANS
+    # Only logged in if there's a points input field
+    # This will be true if we're coming from another game/room/login
+    # Will raise an exception if the required info isn't present, and
+    # send the user back to the login page (see below)
+    user_inventory = game.UserInventory(form)
 
-# Get the planet's description from the file
-description = template.content("room%d" % room_number, room)
+    # Planet class does all the backend stuff.
+    planet = game.Planet(user_inventory)
 
-# Get the rows of the price table
-table_rows = [template.content("price_table_row", row) for row in planet.market()]
+    # Get the planet's description from the file
+    description = template.content("room%d" % room_number, room)
 
-# Get the table using the rows
-table = template.content("price_table", dict(rows = ''.join(table_rows)))
+    # Get the rows of the price table
+    table_rows = [template.content("price_table_row", row) for row in planet.market]
 
-# Get the map for use in the footer
-def room_url(index):
-    if index == 0:
-        return 'http://www.cs.mcgill.ca/~llehne/room-page/room-page.html'
-    elif index == 6:
-        return 'http://cs.mcgill.ca/~ztrifi/myPage.html'
-    else:
-        return 'show.py?room=%d' % index
+    # Get the table using the rows
+    table = template.content("price_table", dict(rows = ''.join(table_rows)))
 
-footer_rows = ""
-for i,r in enumerate(game.Rooms):
-    footer_rows += template.content("footer_row", {'room_name': r['name'], 
-                                                   'image_url': "images/"+r['image']+".jpg", 
-                                                   'room_url':  room_url(i + 1),
-                                                   'room_id': i+1})
-# IF THE USER HAS GOTTEN HERE BY LEGITIMATE MEANS
-# working on this
-# Only logged in if there's a points input field
-# This will be true if we're coming from another game/room/login
-# So it works out, k
-if form.has_key("points"):
+    # Get the map for use in the footer
+    def room_url(index):
+        if index == 0:
+            return 'http://www.cs.mcgill.ca/~llehne/room-page/room-page.html'
+        elif index == 6:
+            return 'http://cs.mcgill.ca/~ztrifi/myPage.html'
+        else:
+            return 'show.py?room=%d' % index
+
+    points_form = user_inventory.render()
+    footer_rows = ""
+    for i,r in enumerate(game.Rooms):
+        footer_rows += template.content("footer_row", {'room_name': r['name'],
+                                                       'image_url': "images/"+r['image']+".jpg",
+                                                       'room_url':  room_url(i + 1),
+                                                       'points_form': points_form})
+
     # Render the room template
-    template.render("room", {'page_title': room['title'], 
-                             'page_name': "room%d" % room_number, 
-                             'description': description, 
-                             'table': table, 
+    template.render("room", {'page_title': room['title'],
+                             'page_name': "room%d" % room_number,
+                             'description': description,
+                             'table': table,
                              # Sanitise shit later whatever
-                             'points': form['points'].value,
+                             'points': user_inventory.points,
                              'footer_rows': footer_rows,
                              'left_url': room_url(room_number-1),
-                             'right_url':room_url(room_number+1)})
-else:
+                             'right_url': room_url(room_number+1),
+                             'points_form': points_form})
+
+except game.MalformedGameFormError:
     # Go back to the login page teehee
     template.render("login", dict(page_title="Login", layout="logged_out"))
